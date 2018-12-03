7684e5f088ed53d99fd49933a5859c3037e09bab
JuanCC90 <35227696+JuanCC90@users.noreply.github.com>
2018-12-03 19:26:05 +0100

    Add files via upload
    Seguimos trabajando con funcionalidades de Angular, pruebas variadas

---
+++ ANGULAR/JS/app.js
@@ -0,0 +1,16 @@
+var app=angular.module('UniversidadApp',[]);
+
+app.controller('profesorCtrl',function(){
+	
+	this.profesor={
+	
+	nombre:"Javier Prada",
+	bio:"Saludos estudiantes,mi nombre es Javier, encantado de conoceros",
+	edad:40,
+	foto:"IMG/javi.jpeg"
+	}
+	
+	
+});
+
+	
